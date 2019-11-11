1fd497cc28702bb247a8f9817b63d588545ee1a3
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-11-11 17:57:08 +0500

    virtual env created

--- BasicFlaskAuth/app.py
+++ BasicFlaskAuth/app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, abort
 import json
 from functools import wraps
-from jose import jwt
+from cryptography import jwt
+# from jose import jwt
 from urllib.request import urlopen
 
 
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
+AUTH0_DOMAIN = 'fsnd-sbajwa.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
+API_AUDIENCE = 'image'
 
 
 class AuthError(Exception):
